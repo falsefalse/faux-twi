9a92ee771b4beea42d153f8d6e14cbff771b3e48
Ilya Furman <smashlong@gmail.com>
2015-04-07 18:31:27 +0300

    Fixture for tweets, WIP on redrawing list after adding new tweet to collection

--- assets/app.js
+++ assets/app.js
@@ -4,41 +4,36 @@ FT.TweetCollection = Backbone.Collection.extend({
   model: FT.TweetModel
 });
 
-// Routes
-FT.App = Backbone.Router.extend({
-  routes: {
-    '': 'index'
-  },
-  index: function() {
-    var allTweets = new FT.TweetCollection([
-     { body: 'some1', timestamp: '2015-04-07T14:47:33.751Z' },
-     { body: 'some2', timestamp: '2015-04-07T14:47:33.752Z' },
-     { body: 'some3', timestamp: '2015-04-07T14:47:33.753Z' },
-     { body: 'some4', timestamp: '2015-04-07T14:47:33.754Z' }
-    ]);
+// Fixtures
+FT.TWEETS = new FT.TweetCollection([
+ { body: 'woot woot', timestamp: '2015-04-07T14:47:33.751Z' },
+ { body: 'moot moot', timestamp: '2015-04-07T14:47:33.752Z' },
+ { body: 'bar baz', timestamp: '2015-04-07T14:47:33.753Z' },
+ { body: 'cut me some slack', timestamp: '2015-04-07T14:47:33.754Z' }
+]);
 
-    new FT.TweetsList({ collection: allTweets });
-  }
-});
 
 // Views
 FT.PostNew = Backbone.View.extend({
   el: '.post-new',
   initialize: function() {
-    console.log(this.$el);
+    console.log('new', this.$el);
   },
   events: {
     'submit': 'addTweet'
   },
   addTweet: function(event) {
     // collect values, create a model, save it
-    var values = this.$el.serializeArray().reduce(function(acc, pair) {
+    var form = this.$el.serializeArray().reduce(function(acc, pair) {
       acc[pair.name] = pair.value;
       return acc;
     }, {});
 
-    values.timestamp = (new Date).toISOString();
-    var newTweet = new FT.TweetModel(values);
+    form.timestamp = (new Date).toISOString();
+    FT.TWEETS.add( new FT.TweetModel(form) );
+
+    FT.app.navigate('', { trigger: true, replace: true });
+
     event.preventDefault();
   }
 });
@@ -46,7 +41,7 @@ FT.PostNew = Backbone.View.extend({
 FT.TweetsList = Backbone.View.extend({
   el: '.tweets-list',
   initialize: function() {
-    console.log(this);
+    console.log('list', this);
     this.template = _.template( $('#tpl-tweet-item').text() );
     this.render();
   },
@@ -61,8 +56,16 @@ FT.TweetsList = Backbone.View.extend({
 });
 
 $(function() {
-  var app = new FT.App();
+  FT.app = new (Backbone.Router.extend({
+    routes: {
+      '': 'index'
+    },
+    index: function() {
+      new FT.TweetsList({ collection: FT.TWEETS });
+      new FT.PostNew();
+    }
+  }));
+
   Backbone.history.start();
 });
-
 
